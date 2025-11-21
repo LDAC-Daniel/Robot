a158dd5f662acf32a176ca41dd0f8eb2e877eb3d
armij <armijoscevallosdaniel@gmail.com>
2025-11-21 13:21:32 +0100

    Enumerado de Orientacion.java creado

---
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Orientacion.java
@@ -0,0 +1,23 @@
+package org.iesalandalus.programacion.robot.modelo;
+
+public enum Orientacion {
+    NORTE("Norte"),
+    NORESTE("Noreste"),
+    ESTE("Este"),
+    SURESTE("Sureste"),
+    SUR("Sur"),
+    SUROESTE("Suroeste"),
+    OESTE("Oeste"),
+    NOROESTE("Noroeste");
+
+    private String cadenaMostrar;
+
+    private Orientacion (String cadenaMostrar){
+        this.cadenaMostrar = cadenaMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return cadenaMostrar;
+    }
+}
